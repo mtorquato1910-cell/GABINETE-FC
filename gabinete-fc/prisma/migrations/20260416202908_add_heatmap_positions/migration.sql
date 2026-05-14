de2bea46bb85d10340a19e21bbb98a392b84ff30
mtorquato1910-cell <mtorquato1910@gmail.com>
2026-05-14 11:02:50 -0300

    feat: migrar Prisma SQLite -> Postgres (Supabase) + remover migrations antigas

--- gabinete-fc/prisma/migrations/20260416202908_add_heatmap_positions/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "behavior_events" ADD COLUMN "posX" REAL;
-ALTER TABLE "behavior_events" ADD COLUMN "posY" REAL;
-
--- CreateIndex
-CREATE INDEX "behavior_events_pageUrl_eventType_idx" ON "behavior_events"("pageUrl", "eventType");
